ba7c73e3aad0b4b3dde5e090e5055d19864d1060
Antoni Pater <135012541+antoniopater@users.noreply.github.com>
2025-05-10 15:47:40 +0200

    Time: 11 ms (51.61%), Space: 19 MB (48.88%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,6 +1,7 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        count = Counter(s)
-        sorted_s = sorted(count.items(),key=lambda x:x[1],reverse=True)
-        result = "".join([char*freq for char,freq in sorted_s])
-        return(result)
+        count = defaultdict(int)
+        for ch in s:
+            count[ch]+=1
+        chars_sorted = sorted(count, key=count.get, reverse=True)
+        return "".join(ch*count[ch] for ch in chars_sorted)
